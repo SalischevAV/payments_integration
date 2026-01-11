80cad722461775c1c2373cf81d0c21f13995f90f
SalischevAV <devorer77@gmail.com>
2026-01-11 14:37:56 +0100

    fix: test version (#7)

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	const config = app.get(ConfigService);
 	const logger = new Logger('Bootstrap');
+	const config = app.get(ConfigService);
 
 	app.enableCors(getCorsConfig(config));
 	app.useGlobalPipes(new ValidationPipe());
